9e783a1951ee9b5f1c00cc0eec638fa49f6b55fd
Brian Otieno <gebryo@intelligencia.com>
2021-08-04 20:27:45 +0300

    code base

---
+++ app/src/main/java/com/royalai/tourguide/TourGuideRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.royalai.tourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TourGuideRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_guide_register);
+    }
+}
